3abd6f4c88d9973b2a8d44a855d3e3e579a3c2d1
raufkali <ahmadraufbd@gmail.com>
2025-08-20 13:13:43 +0500

    update transaction.jsx for new change

--- renderer/src/components/Transactions.jsx
+++ renderer/src/components/Transactions.jsx
@@ -6,32 +6,37 @@ import RecieveForm from "./tForms/RecieveForm";
 import Journal from "./Journal";
 
 const Transactions = () => {
-  let [type, setType] = useState("");
+  const [type, setType] = useState("");
+
   return (
     <div className="main-content py-4 pe-4 text-center">
-      <div class="row bg-light p-4 mb-5">
-        <div class="col-12 card shadow-sm p-4 border-0 bg-white d-flex justify-content-center align-items-center">
+      <div className="row bg-light p-4 mb-5">
+        <div className="col-12 card shadow-sm p-4 border-0 bg-white d-flex justify-content-center align-items-center">
           <h4>Create New Transaction</h4>
+
           <select
-            class="form-select shadow-sm bg-white w-25 text-center my-2 "
-            onChange={(e) => {
-              setType(e.target.value);
-            }}
+            className="form-select shadow-sm bg-white w-25 text-center my-2"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
           >
-            <option defaultValue="Select Type">Select Type</option>
+            <option value="">Select Type</option>
             <option value="sell">Sell</option>
             <option value="buy">Buy</option>
             <option value="send">Send</option>
             <option value="recieve">Recieve</option>
           </select>
-          <div class="row">
-            <div class="col-12">{type === "sell" && <SellForm />}</div>
-            <div class="col-12">{type === "buy" && <BuyForm />}</div>
-            <div class="col-12">{type === "send" && <SendForm />}</div>
-            <div class="col-12">{type === "recieve" && <RecieveForm />}</div>
+
+          <div className="row">
+            <div className="col-12">{type === "sell" && <SellForm />}</div>
+            <div className="col-12">{type === "buy" && <BuyForm />}</div>
+            <div className="col-12">{type === "send" && <SendForm />}</div>
+            <div className="col-12">
+              {type === "recieve" && <RecieveForm />}
+            </div>
           </div>
         </div>
-        <div class=" p-4 shadow-sm mt-4 bg-white border-2 ">
+
+        <div className="p-4 shadow-sm mt-4 bg-white border-2">
           <h3 className="mb-3">All Transactions</h3>
           <Journal />
         </div>
